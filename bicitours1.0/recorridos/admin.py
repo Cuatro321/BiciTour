b35c923c0acabc7c63877fe8ff335b1579b7613f
YunuenZaga <abigailyzacariasg@gmail.com>
2025-07-11 15:31:56 -0600

    App recorridos

--- bicitours1.0/recorridos/admin.py
+++ bicitours1.0/recorridos/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Recorrido
 
+#Abigail Yunuen Zacarías García
+#Registro del modelo Recorrido en el panel de administración
+#Personalización y visualización en la lista de objetos
 @admin.register(Recorrido)
 class RecorridoAdmin(admin.ModelAdmin):
     list_display = ('ciudad', 'fecha', 'realizado', 'costo')
--- bicitours1.0/recorridos/models.py
+++ bicitours1.0/recorridos/models.py
@@ -13,3 +13,23 @@ descripcion
 foto
 realizado
 """
+
+#Abigail Yunuen Zacarías García
+
+#Modelo Recorrido donde se muestra el recorrido que se realizó con los detalles correspondientes
+#Tipos de campos utilizados CharField, DateField, TimeField, DecimalField, etc
+class Recorrido(models.Model):
+    fecha = models.DateField()
+    hora = models.TimeField()
+    estado = models.CharField("Estado", max_length=1000)
+    ciudad = models.CharField("Ciudad", max_length=100)
+    km =models.DecimalField("Distancia (km)", max_digits=5, decimal_places=2)
+    tiempo_estimado = models.DurationField("Tiempo estimado")
+    punto_inicio = models.DurationField("Tiempo estimado")
+    costo = models.DecimalField("Costo ($)", max_digits=8, decimal_places=2)
+    descripcion = models.TextField("Descripción")
+    foto = models.ImageField("Imagen del recorrido", upload_to='fotos_recorridos/')
+    realizado = models.BooleanField("¿Ya se realizó?", default=False)
+
+def __str__(self):
+    return f"{self.ciudad} - {self.fecha.strftime('%d/%m/%Y')}"
