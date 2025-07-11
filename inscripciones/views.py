043f44125a4fef4fc58c29ede734477910dc10d4
PabloPena19975 <juanpayepa@gmail.com>
2025-07-11 16:59:57 -0600

    Incripciones
    Añadi el apartado de Incripciones al proyecto, igual les avisare por what's app

---
+++ inscripciones/views.py
@@ -0,0 +1,24 @@
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.views.generic import TemplateView
+from .models import Inscripcion
+
+"""
+Clases principales crear y el de gracias a posibles cambios
+"""
+
+class InscripcionCreateView(CreateView):
+    model = Inscripcion
+    fields = ['recorrido', 'nombre', 'correo', 'telefono', 'ciudad', 'estado']
+    template_name = 'inscripciones/inscripcion_form.html'
+    success_url = reverse_lazy('inscripciones:gracias')
+
+    def get_initial(self):
+        initial = super().get_initial()
+        recorrido_id = self.request.GET.get('recorrido')
+        if recorrido_id:
+            initial['recorrido'] = recorrido_id
+        return initial
+
+class GraciasTemplateView(TemplateView):
+    template_name = 'inscripciones/gracias.html'
