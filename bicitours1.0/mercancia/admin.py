6f3db58fd63e4d945249f67d06acfb53e36c97d1
Renato <jhoni.dragon1234@gmail.com>
2025-07-11 18:56:30 -0600

    Actualización Renato

--- bicitours1.0/mercancia/admin.py
+++ bicitours1.0/mercancia/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Prducto
 
+admin.site.register(Producto)
 #REGISTRAR PRODUCTOS  
--- bicitours1.0/mercancia/models.py
+++ bicitours1.0/mercancia/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 
-
 """Modelo mercancia
     nombre
     descripcion
     precio
     imagen
     disponible
-
 """
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=8, decimal_places=2)
+    imagen = models.ImageField(upload_to='productos/')
+    disponible = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
+
+
+
